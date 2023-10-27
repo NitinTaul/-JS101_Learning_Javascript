8a0dfbdd159b8c6f9c6fc51ec66d35904b45c867
Nitin Taul <nitin8097270813@gmail.com>
2023-10-27 16:32:37 +0530

    Day 1 Task

---
+++ Intro to Js/1.js
@@ -0,0 +1,5 @@
+let name="Masai School";
+console.log(name);
+
+let Slogan="A Transformation in Education";
+console.log(Slogan);
---
+++ Intro to Js/3.js
@@ -0,0 +1,26 @@
+let name="𝓝𝓲𝓽𝓲𝓷 𝓣𝓪𝓾𝓵";
+let School="𝕾𝖍𝖗𝖊𝖊 𝕲𝖆𝖓𝖊𝖘𝖍 𝖁𝖎𝖉𝖞𝖆 𝕸𝖆𝖓𝖉𝖎𝖗";
+let grade="A+";
+let section="B";
+let roll_no=20;
+let Marathi=97;
+let Hindi=95;
+let English=87;
+
+console.log("#########    𝓡𝓮𝓹𝓸𝓻𝓽 𝓬𝓪𝓻𝓭   #########")
+console.log("#    𝓢𝓱𝓻𝓮𝓮 𝓖𝓪𝓷𝓮𝓼𝓱 𝓥𝓲𝓭𝔂𝓪 𝓜𝓪𝓷𝓭𝓲𝓻     #");
+console.log("#                                  #")
+console.log("# 𝑆𝑡𝑢𝑑𝑒𝑛𝑡 𝑁𝑎𝑚𝑒:",name, "        #")
+console.log("# 𝑆𝑒𝑐𝑡𝑖𝑜𝑛:",section,"                      #")
+console.log("# 𝑅𝑜𝑙𝑙 𝑁𝑜:",roll_no,"                     #")
+console.log("#                                  #")
+
+console.log("#  𝓢𝓾𝓫𝓳𝓮𝓬𝓽   ","        𝓜𝓪𝓻𝓴𝓼","       #")
+console.log("#                                  #")
+console.log("#  𝑀𝑎𝑟𝑎𝑡ℎ𝑖 --         ",Marathi,"         #")
+console.log("#  𝐻𝑖𝑛𝑑𝑖 --           ",Hindi,"         #")
+console.log("#  𝐸𝑛𝑔𝑙𝑖𝑠ℎ --         ",English,"         #")
+console.log("#                                  #")
+console.log("#                   𝓖𝓻𝓪𝓭𝓮:",grade,"     #")
+console.log("#                                  #")
+console.log("#########       X-X-X        #######")
